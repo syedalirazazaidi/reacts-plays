01bb06467f9aba896d41b2fcbdc931fdc9997741
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-03-15 00:43:29 +0500

    changes in response

--- task_manager/server/controllers/tasks.js
+++ task_manager/server/controllers/tasks.js
@@ -37,7 +37,8 @@ const deleteTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task with id : ${taskID}` })
     }
-    res.status(200).json({ task })
+    // res.status(200).json({ task })
+    res.status(200).json({ success: 'Deleted successfully', data: { task } })
   } catch (error) {
     res.status(500).json({ msg: error })
   }
@@ -55,7 +56,8 @@ const updateTask = async (req, res) => {
     return res.status(404).json({ msg: `No task with id : ${taskID}` })
   }
 
-  res.status(200).json({ task })
+  // res.status(200).json({ task })
+  res.status(200).json({ success: 'Updated successfully', data: { task } })
 }
 
 module.exports = {
