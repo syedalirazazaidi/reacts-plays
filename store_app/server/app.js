177b3cca4adc57e46687e905aae68f82be29adc2
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-03-19 01:46:14 +0500

    db connected

--- store_app/server/app.js
+++ store_app/server/app.js
@@ -1,1 +1,33 @@
-console.log('hi deasadasdsa asdr')
+require('dotenv').config()
+const express = require('express')
+
+const cors = require('cors')
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+const PORT = process.env.PORT | 5000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`),
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
