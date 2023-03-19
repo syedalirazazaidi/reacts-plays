c6edc076d0d862b3dd9708fe1426fff982261674
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-03-19 23:08:39 +0500

    populate data

--- store_app/server/controllers/products.js
+++ store_app/server/controllers/products.js
@@ -1,9 +1,9 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price: { $gt: 30 } })
-    .sort('price')
-    .select('name price')
+  const products = await Product.find({ featured: true })
+  // .sort('price')
+  // .select('name price')
 
   res.status(200).json({ products, nbHits: products.length })
 }
