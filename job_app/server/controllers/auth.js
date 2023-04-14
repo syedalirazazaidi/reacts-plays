0f619f29da2c56c5d97d01b6e1b48f276de37473
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-15 02:01:21 +0500

    add bcrypt and hashsalt

--- job_app/server/controllers/auth.js
+++ job_app/server/controllers/auth.js
@@ -1,18 +1,18 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, UnauthenticatedError } = require('../errors')
-
+const jwt = require('jsonwebtoken')
 const register = async (req, res) => {
-  const { name, email, password } = req.body
-  if (!name || !email || !password) {
-    return res
-      .status(400)
-      .json({ error: 'Please provide name,email and password' })
-  }
+  // const { name, email, password } = req.body
+  // if (!name || !email || !password) {
+  //   return res
+  //     .status(StatusCodes.CREATED)
+  //     .json({ error: 'Please provide name,email and password' })
+  // }
 
   const user = await User.create({ ...req.body })
-  // const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
--- job_app/server/models/User.js
+++ job_app/server/models/User.js
@@ -25,24 +25,24 @@ const UserSchema = new mongoose.Schema({
   },
 })
 
-// UserSchema.pre('save', async function () {
-//   const salt = await bcrypt.genSalt(10)
-//   this.password = await bcrypt.hash(this.password, salt)
-// })
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
-// UserSchema.methods.createJWT = function () {
-//   return jwt.sign(
-//     { userId: this._id, name: this.name },
-//     process.env.JWT_SECRET,
-//     {
-//       expiresIn: process.env.JWT_LIFETIME,
-//     },
-//   )
-// }
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    // {
+    //   expiresIn: process.env.JWT_LIFETIME,
+    // },
+  )
+}
 
-// UserSchema.methods.comparePassword = async function (canditatePassword) {
-//   const isMatch = await bcrypt.compare(canditatePassword, this.password)
-//   return isMatch
-// }
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password)
+  return isMatch
+}
 
 module.exports = mongoose.model('User', UserSchema)
