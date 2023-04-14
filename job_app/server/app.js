f52131c7ab2fc014f7311e438c006d7632c0f503
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-14 12:03:40 +0500

    mongodb connected

--- job_app/server/app.js
+++ job_app/server/app.js
@@ -1,2 +1,23 @@
-// uEvqCjerbXmRMSEu
-// mongodb+srv://job_app:<password>@cluster0.ydckyv6.mongodb.net/?retryWrites=true&w=majority
+const express = require('express')
+
+const cors = require('cors')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT | 5000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`),
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
