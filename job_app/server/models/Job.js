f9c8e4aa8fc0daa20982fb15ff8c9be25a5703b1
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-17 02:38:04 +0500

    create job model

--- job_app/server/models/Job.js
+++ job_app/server/models/Job.js
@@ -1,74 +1,29 @@
-const Job = require('../models/Job')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError, NotFoundError } = require('../errors')
+const mongoose = require('mongoose')
 
-const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
-}
-const getJob = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: jobId },
-  } = req
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true },
+)
 
-  const job = await Job.findOne({
-    _id: jobId,
-    createdBy: userId,
-  })
-  if (!job) {
-    throw new NotFoundError(`No job with id ${jobId}`)
-  }
-  res.status(StatusCodes.OK).json({ job })
-}
-
-const createJob = async (req, res) => {
-  req.body.createdBy = req.user.userId
-  const job = await Job.create(req.body)
-  res.status(StatusCodes.CREATED).json({ job })
-}
-
-const updateJob = async (req, res) => {
-  const {
-    body: { company, position },
-    user: { userId },
-    params: { id: jobId },
-  } = req
-
-  if (company === '' || position === '') {
-    throw new BadRequestError('Company or Position fields cannot be empty')
-  }
-  const job = await Job.findByIdAndUpdate(
-    { _id: jobId, createdBy: userId },
-    req.body,
-    { new: true, runValidators: true },
-  )
-  if (!job) {
-    throw new NotFoundError(`No job with id ${jobId}`)
-  }
-  res.status(StatusCodes.OK).json({ job })
-}
-
-const deleteJob = async (req, res) => {
-  const {
-    user: { userId },
-    params: { id: jobId },
-  } = req
-
-  const job = await Job.findByIdAndRemove({
-    _id: jobId,
-    createdBy: userId,
-  })
-  if (!job) {
-    throw new NotFoundError(`No job with id ${jobId}`)
-  }
-  res.status(StatusCodes.OK).send()
-}
-
-module.exports = {
-  createJob,
-  deleteJob,
-  getAllJobs,
-  updateJob,
-  getJob,
-}
+module.exports = mongoose.model('Job', JobSchema)
