22359a3d7c4c95f6f4c169339189d46109955bf2
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-16 13:47:43 +0500

    login

--- job_app/server/controllers/auth.js
+++ job_app/server/controllers/auth.js
@@ -19,15 +19,23 @@ const login = async (req, res) => {
   const { email, password } = req.body
 
   if (!email || !password) {
-    throw new BadRequestError('Please provide email and password')
+    // throw new BadRequestError('Please provide email and password')
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ error: 'Please provide email and password' })
   }
   const user = await User.findOne({ email })
   if (!user) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ error: 'Invalid Credentials' })
   }
   const isPasswordCorrect = await user.comparePassword(password)
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError('Invalid Credentials')
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ error: 'Invalid Credentials' })
+    // throw new UnauthenticatedError('Invalid Credentials')
   }
   // compare password
   const token = user.createJWT()
--- job_app/server/models/User.js
+++ job_app/server/models/User.js
@@ -40,8 +40,8 @@ UserSchema.methods.createJWT = function () {
   )
 }
 
-UserSchema.methods.comparePassword = async function (canditatePassword) {
-  const isMatch = await bcrypt.compare(canditatePassword, this.password)
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password)
   return isMatch
 }
 
