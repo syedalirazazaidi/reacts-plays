d893b0fa8300fe49017c08b255557a2ac993708f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-04-20 20:33:05 +0500

    add react-router-dom and tailwind for css

--- job_app/client/src/common/routers/RootRouter.tsx
+++ job_app/client/src/common/routers/RootRouter.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function RootRouter() {
-  return <div>RootRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <p className="text-3xl font-bold underline bg-red-300">
+              Hello new world
+            </p>
+          }
+        />
+        <Route path="*" element={<p>Not found 404</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
