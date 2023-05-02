1f6d3a855867c80ab1946cd7b3cdd9265f6aa489
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-05-02 20:39:17 +0500

    completed folder structure

--- job_app/client/src/main.tsx
+++ job_app/client/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppProvider from "./common/providers/AppProvider";
-
+import 'remixicon/fonts/remixicon.css'
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
