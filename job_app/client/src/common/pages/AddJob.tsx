0b48944777b504d0d2626e4c0cee48fa6ec13037
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-05-03 03:08:34 +0500

    add add-jobs field

--- job_app/client/src/common/pages/AddJob.tsx
+++ job_app/client/src/common/pages/AddJob.tsx
@@ -3,9 +3,84 @@ import MainLayout from "../layout/MainLayout";
 
 function AddJob() {
   return (
-    <MainLayout>
-      <div>AddJob</div>
-    </MainLayout>
+    <div className="bg-white rounded-md shadow-lg p-4  ">
+      {/* <h2 className="text-lg font-semibold mb-2">Card Title</h2> */}
+      <form className="flex flex-wrap gap-4 justify-start mx-24 my-8">
+        <div className="mb-4 ">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="input1"
+          >
+            Input 1
+          </label>
+          <input
+            className="w-80 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="input1"
+            type="text"
+            placeholder="Enter input 1"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="input2"
+          >
+            Input 2
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="input2"
+            type="text"
+            placeholder="Enter input 2"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="input3"
+          >
+            Input 3
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="input3"
+            type="text"
+            placeholder="Enter input 3"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="input3"
+          >
+            Input 3
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="input3"
+            type="text"
+            placeholder="Enter input 3"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="input3"
+          >
+            Input 3
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="input3"
+            type="text"
+            placeholder="Enter input 3"
+          />
+        </div>
+      </form>
+    </div>
+    // <MainLayout>
+    //   <div>AddJob</div>
+    // </MainLayout>
   );
 }
 
