2ee0f0d87fb2febe3361ffd41def9b4eaac03971
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-05-06 12:09:39 +0500

    get data from the field

--- job_app/client/src/common/pages/AddJob.tsx
+++ job_app/client/src/common/pages/AddJob.tsx
@@ -1,9 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, ChangeEvent, FormEvent } from "react";
 import { SidebarContext } from "../../contexts/SidebarContext";
 import MainLayout from "../layout/MainLayout";
+import { AddJobType } from "../types/type";
 
 function AddJob() {
+  const [addJob, setAddJob] = useState<AddJobType>({
+    position: "",
+    company: "",
+    location: "",
+    status: "",
+    job_type: "",
+  });
+
   const { isSetOpen, isOpen }: any = useContext(SidebarContext);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    setAddJob((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(addJob);
+  };
+
   return (
     <div
       className={` shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ${
@@ -13,7 +36,10 @@ function AddJob() {
       }`}
     >
       <p className="ml-24 mt-3 text-3xl">Add Job</p>
-      <form className="flex flex-wrap gap-4 content-center justify-start mx-24 my-8">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-wrap gap-4 content-center justify-start mx-24 my-8"
+      >
         <div className="mb-4 ">
           <label
             className="block text-gray-700 text-sm font-semibold mb-2"
@@ -25,9 +51,13 @@ function AddJob() {
             className={` shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ${
               !isOpen ? "w-80" : "w-96"
             }`}
-            id="input1"
+            value={addJob.position}
+            onChange={handleChange}
+            name="position"
+            id="position"
             type="text"
             placeholder="position"
+            required
           />
         </div>
         <div className="mb-4">
@@ -41,15 +71,19 @@ function AddJob() {
             className={` shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ${
               !isOpen ? "w-80" : "w-96"
             }`}
-            id="input2"
+            id="company"
+            value={addJob.company}
+            onChange={handleChange}
             type="text"
             placeholder="company"
+            required
+            name="company"
           />
         </div>
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-semibold mb-2"
-            htmlFor="input3"
+            htmlFor="location"
           >
             Job Location
           </label>
@@ -57,15 +91,19 @@ function AddJob() {
             className={` shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ${
               !isOpen ? "w-80" : "w-96"
             }`}
-            id="input3"
+            id="location"
             type="text"
+            name="location"
+            value={addJob.location}
+            onChange={handleChange}
             placeholder="job location"
+            required
           />
         </div>
 
         <div className="mb-4">
           <label
-            htmlFor="countries"
+            htmlFor="status"
             className="block text-gray-700 text-sm font-semibold mb-2"
           >
             Status
@@ -75,7 +113,12 @@ function AddJob() {
             className={`shadow  border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ${
               !isOpen ? "w-80" : "w-96"
             }`}
+            name="status"
+            value={addJob.status}
+            onChange={handleChange}
+            required
           >
+            <option value="">Select a status</option>
             <option value="declined">declined</option>
             <option value="interview">Interview</option>
             <option value="FR">pending</option>
@@ -89,11 +132,16 @@ function AddJob() {
             Job Type
           </label>
           <select
-            id="status"
+            id="job_type"
             className={`shadow  border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ${
               !isOpen ? "w-80" : "w-96"
             }`}
+            name="job_type"
+            value={addJob.job_type}
+            onChange={handleChange}
+            required
           >
+            <option value="">Select a job type</option>
             <option value="declined">full time</option>
             <option value="interview">part time</option>
             <option value="FR">remote</option>
@@ -110,6 +158,7 @@ function AddJob() {
           Clear
         </button>
         <button
+          type="submit"
           className={`{
           bg-amber-600  h-10 mt-7 hover:bg-amber-500 text-white font-medium  rounded" ${
             !isOpen ? "w-36 rounded ml-4" : "w-44 rounded ml-4"
--- job_app/client/src/common/types/type.ts
+++ job_app/client/src/common/types/type.ts
@@ -1,1 +1,8 @@
 export type ISHow = boolean;
+export interface AddJobType {
+  position: string;
+  company: string;
+  location: string;
+  status: string;
+  job_type: string;
+}
