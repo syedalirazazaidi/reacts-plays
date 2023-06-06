d5481252e2246ebb5a54080bdc71df97ad479d2b
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-07 02:06:35 +0500

    changes in profile section componet

--- job_app/client/src/common/pages/Profile.tsx
+++ job_app/client/src/common/pages/Profile.tsx
@@ -1,13 +1,23 @@
 import React, { useContext } from "react";
 import { SidebarContext } from "../../contexts/SidebarContext";
+import { ToastContext } from "../../contexts/ToastContext";
 
 function Profile() {
   const { isSetOpen, isOpen, setEditFormData }: any =
     useContext(SidebarContext);
+  const { showToast }: any = useContext(ToastContext);
+
+  const handleSubmit = (e: any) => {
+    showToast("Working on profile section thankyou", "success");
+    e.preventDefault();
+  };
   return (
     <div className="bg-white rounded-md shadow-lg p-4  ">
       <p className="ml-20 mt-3 text-3xl">Profile</p>
-      <form className="flex flex-wrap gap-4 justify-start mx-24 my-8">
+      <form
+        className="flex flex-wrap gap-4 justify-start mx-24 my-8"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4 ">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
