b52f038825a0d96595efcab3451e8940733bac7f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-13 21:12:51 +0500

    more on ISR pages

--- nextjsplayground/pract01/lib/getUserPosts.tsx
+++ nextjsplayground/pract01/lib/getUserPosts.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export default async function getUserPost(userId: any) {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+    { next: { revalidate: 60 } }
   );
 
   if (!res.ok) {
