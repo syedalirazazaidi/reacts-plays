c0f97d76947c5279ba48f7e46c5bfa67b5cfc6ff
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-16 21:42:10 +0500

    add new api routes

--- nextjsplayground/apirouting/src/app/api/echo/route.ts
+++ nextjsplayground/apirouting/src/app/api/echo/route.ts
@@ -1,4 +1,8 @@
 import { NextResponse } from "next/server";
 export async function GET(request: Request) {
-  return NextResponse.json({ Hello: ",Next.js!" });
+  const { searchParams } = new URL(request.url);
+
+  const name = searchParams.get("name");
+
+  return NextResponse.json({ Hello: ",Next.js! Echo", name });
 }
