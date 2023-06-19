707c8f345e0d60b181ba91a6d01df6b480abc003
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-19 21:28:31 +0500

    miner changes in css

--- nextjsplayground/apirouting/src/app/api/todo/route.ts
+++ nextjsplayground/apirouting/src/app/api/todo/route.ts
@@ -9,6 +9,30 @@ export async function GET() {
 
   return NextResponse.json(todos);
 }
+export async function PUT(request: Request) {
+  const { userId, id, title, completed } = await request.json();
+
+  if (!userId || !id || !title || typeof completed !== "boolean")
+    return NextResponse.json({ message: "Missing required data" });
+
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "API-Key": API_KEY,
+    },
+    body: JSON.stringify({
+      userId,
+      title,
+      completed,
+    }),
+  });
+
+  const updatedTodo = await res.json();
+
+  return NextResponse.json(updatedTodo);
+}
+
 export async function DELETE(request: Request) {
   const { id } = await request.json();
 
