897d44e4fe6579ef30863ce818cde91ea629d2eb
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-20 23:23:26 +0500

    changes in UI

--- nextjsplayground/apirouting/src/app/page.tsx
+++ nextjsplayground/apirouting/src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <div className=" bg-red-200 w-full  p-4">
         <div className="">
           {/* width:'max-content' */}
-          <p className="border border-indigo-600 w-32 max-w-sm">
+          {/* <p className="border border-indigo-600 w-32 max-w-sm">
             ho do you do ho do you do ho do you do ho do you do ho do you do ho
             do you do
-          </p>
+          </p> */}
         </div>
       </div>
       {/* <div className="w-1/3 aspect-1/1 bg-green-300 mb-24">box</div> */}
