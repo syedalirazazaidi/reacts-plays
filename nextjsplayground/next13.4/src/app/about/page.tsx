9d6bc3936314fce301f14adf1c29576d9bf0837d
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-21 22:33:56 +0500

    completed header

---
+++ nextjsplayground/next13.4/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      about
+    </div>
+  );
+}
---
+++ nextjsplayground/next13.4/src/app/movie/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Movie() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Movie
+    </div>
+  );
+}
