27503e110372e84bb3fb488f07c9e62d80fc6043
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-23 23:14:38 +0500

    add not-found component

---
+++ nextjsplayground/next13.4/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="container">
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
