bda3de33c2ae1b5fbf8262ad56347cfa0351cb18
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-06-23 23:08:05 +0500

    add loader

--- nextjsplayground/next13.4/src/app/movie/[id]/page.tsx
+++ nextjsplayground/next13.4/src/app/movie/[id]/page.tsx
@@ -1,7 +1,34 @@
+// import React from "react";
+
+// function page({ params }: any) {
+
+//   return <div>I m dynamic movies,{id}</div>;
+// }
+
+// export default page;
 import React from "react";
 
-function page() {
-  return <div>I m dynamic movies</div>;
-}
+export default async function page({ params }: any) {
+  const { id } = params;
+  const url: any = `https://netflix54.p.rapidapi.com/title/details/?ids=${id}&lang=en`;
 
-export default page;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "cc87d10f33mshabe1450080883edp10c109jsn3285bb322b99",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {data &&
+        data?.map((item: any, index: any) => (
+          <div key={item?.details?.synopsis} className="bg-white p-4 shadow">
+            <p className="text-xl font-bold ">{item?.details?.synopsis}</p>
+          </div>
+        ))}
+    </div>
+  );
+}
--- nextjsplayground/next13.4/src/app/movie/page.tsx
+++ nextjsplayground/next13.4/src/app/movie/page.tsx
@@ -2,7 +2,13 @@ import React from "react";
 // import getAllmovie from "../../app/lib/getAllmovie";
 import MovieCard from "../components/MovieCard";
 export default async function Movie() {
-  const url: any = process.env.RAPID_KEY;
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hi!");
+    }, 3000);
+  });
+  const url: any =
+    "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=10&limit_suggestions=20&lang=en";
 
   const options = {
     method: "GET",
