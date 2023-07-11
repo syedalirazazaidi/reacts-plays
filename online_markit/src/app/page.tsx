85d54c99ff77099faf2556b47113ed44060e8efa
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-11 20:26:46 +0500

    testing in shadcn

--- online_markit/src/app/page.tsx
+++ online_markit/src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from 'next/image'
+import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     hi this is ali
-    </main>
-  )
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
 }
