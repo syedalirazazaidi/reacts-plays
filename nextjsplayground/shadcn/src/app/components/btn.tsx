cdc3796fd299d44b0f2c99a580e6e4e802468d55
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-14 20:49:24 +0500

    changes styling

--- nextjsplayground/shadcn/src/app/components/btn.tsx
+++ nextjsplayground/shadcn/src/app/components/btn.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function HomeButton() {
   return (
     <div>
-      <Button>CLick ME</Button>
+      <Button variant="destructive">CLick ME</Button>
     </div>
   );
 }
