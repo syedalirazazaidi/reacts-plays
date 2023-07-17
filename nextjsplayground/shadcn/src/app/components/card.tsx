9049c0e92b6adccd9c0c77eb4063041918757fc8
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-17 22:08:51 +0500

    set globals.css file and configure it

---
+++ nextjsplayground/shadcn/src/app/components/card.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function CardComp() {
+  return (
+    <div>
+      <Card className="bg-gray-600 col">
+        <CardHeader>
+          <CardTitle>Card Title Component</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
--- nextjsplayground/shadcn/src/app/page.tsx
+++ nextjsplayground/shadcn/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HomeButton from "./components/btn";
+import CardComp from "./components/card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hi this is developer
       <HomeButton />
+      <CardComp />
     </main>
   );
 }
