411242e9e4ad0233e1b3f45e1d82fae6458ecb8c
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-19 01:24:58 +0500

    add nested route

--- nextjsplayground/next-shadcn/src/app/page.tsx
+++ nextjsplayground/next-shadcn/src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
 import Image from "next/image";
+import { ButtonDemo } from "../componentUI/buttonUI";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [first, setfirst] = useState("second");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="h-screen bg-gray-50 w-screen">
       hi this is next-shadcn
+      {first}
+      <ButtonDemo />
     </main>
   );
 }
