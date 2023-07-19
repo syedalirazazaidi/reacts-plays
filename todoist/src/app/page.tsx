8aef59a716a19a41eaca79bc33b9cceb4db0ea09
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-19 22:19:11 +0500

    test shadcn in nextjs app

--- todoist/src/app/page.tsx
+++ todoist/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { ButtonDemo } from "./testcomponents/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       hi developer
+      <ButtonDemo />
     </main>
-  )
+  );
 }
