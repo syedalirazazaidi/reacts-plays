c6f86218058dae1be22b831650cd0254779c050b
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-21 23:42:39 +0500

    gitignore file

--- todoist/src/app/api/auth/[...nextauth]/route.ts
+++ todoist/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import { connectToDB } from "@/lib/database";
+import User from "@/models/user";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 interface OAuthConfig {
@@ -16,8 +17,10 @@ const handler = NextAuth({
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
+      const sessionUser = await User.findOne({
+        email: session?.user?.email ?? "",
+      });
+      // session.user.id = sessionUser._id.toString();
 
       return session;
     },
@@ -26,19 +29,19 @@ const handler = NextAuth({
         await connectToDB();
 
         // check if user already exists
-        const userExists = await User.findOne({ email: profile.email });
+        const userExists = await User.findOne({ email: profile?.email });
 
         // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
-            email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.picture,
+            email: profile?.email,
+            // username: profile?.name.replace(" ", "").toLowerCase() ?? "",
+            // image: profile.picture ?? "",
           });
         }
 
         return true;
-      } catch (error) {
+      } catch (error: any) {
         console.log("Error checking if user exists: ", error.message);
         return false;
       }
