b5c28276c9e4752dd7da8d485d540ff6af488735
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-28 21:46:11 +0500

    add cancel button

--- todoist/src/commons/Form.tsx
+++ todoist/src/commons/Form.tsx
@@ -72,51 +72,31 @@ export function PromptFrom({
               </span>
             </label>
           </div>
-          <Button
-            type="submit"
-            className="bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-teal-600 focus:outline-none focus:bg-blue-600 w-full"
-          >
-            Submit
-          </Button>
+          <div className=" mx-3 mb-5 gap-4 text-center">
+            <Button
+              type="submit"
+              className="bg-teal-500 text-white py-2 px-4  rounded-md hover:bg-teal-600 focus:outline-none focus:bg-blue-600 w-1/2"
+            >
+              Submit
+            </Button>
+            <div className="text-right mt-[-37px]">
+              <Link
+                href="/"
+                className="text-gray-500 hover:rounded-sm hover:border-red-500 hover:py-2 hover:px-1 hover:text-red-400 focus:outline-none focus:bg-blue-600 text-sm text-right border border-transparent hover:border"
+              >
+                Cancel
+              </Link>
+
+              {/* <Link
+                href="/"
+                className="text-gray-500 hover:rounded-sm hover:border-red-500  hover:text-red-400  focus:outline-none focus:bg-blue-600 text-sm  text-right"
+              >
+                Cancel
+              </Link> */}
+            </div>
+          </div>
         </div>
       </form>
-
-      {/* <form onSubmit={handleSubmit}>
-        <div className="w-1/2 p-4 mx-auto text-center my-[70px]">
-          <div className="mb-4">
-            <Input
-              value={post.tag}
-              onChange={(e) => setPost({ ...post, tag: e.target.value })}
-              type="text"
-              placeholder="#Tag"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <Textarea
-              value={post.prompt}
-              onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-              required
-              placeholder="Type your message here."
-            />
-            <label
-              htmlFor="message"
-              className="block font-semibold text-sm text-gray-700"
-            >
-              Field of Prompt{" "}
-              <span className=" text-sm text-start">
-                (#product, #webdevelopment, #idea, etc.)
-              </span>
-            </label>
-          </div>
-          <Button
-            type="submit"
-            className="bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-          >
-            Submit
-          </Button>
-        </div>
-      </form> */}
     </>
   );
 }
