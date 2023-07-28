484f241488e77f4a254fe3f9aa2099499e0189c6
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-28 19:27:28 +0500

    responsive form in all mobile screen size

--- todoist/src/commons/Form.tsx
+++ todoist/src/commons/Form.tsx
@@ -42,6 +42,46 @@ export function PromptFrom({
   return (
     <>
       <form onSubmit={handleSubmit}>
+        <div className="p-4 text-center md:w-1/2 md:mx-auto md:text-left md:my-70">
+          <div className="mb-4">
+            <Input
+              value={post.tag}
+              onChange={(e) => setPost({ ...post, tag: e.target.value })}
+              type="text"
+              placeholder="#Tag"
+              required
+              className="w-full"
+            />
+          </div>
+          <div className="mb-4">
+            <Textarea
+              value={post.prompt}
+              onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+              required
+              placeholder="Type your message here."
+              className="w-full"
+              rows={4}
+            />
+            <label
+              htmlFor="message"
+              className="block font-semibold text-[10px] text-gray-700"
+            >
+              Field of Prompt{" "}
+              <span className=" block md:inline text-start">
+                (#product, #webdevelopment, #idea, etc.)
+              </span>
+            </label>
+          </div>
+          <Button
+            type="submit"
+            className="bg-teal-500 text-white py-2 px-4 rounded-md hover:bg-teal-600 focus:outline-none focus:bg-blue-600 w-full"
+          >
+            Submit
+          </Button>
+        </div>
+      </form>
+
+      {/* <form onSubmit={handleSubmit}>
         <div className="w-1/2 p-4 mx-auto text-center my-[70px]">
           <div className="mb-4">
             <Input
@@ -76,7 +116,7 @@ export function PromptFrom({
             Submit
           </Button>
         </div>
-      </form>
+      </form> */}
     </>
   );
 }
