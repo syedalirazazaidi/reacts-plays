211f1b754435ccc154b29036685a7948daea9422
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-07-30 21:37:49 +0500

    render data

--- todoist/src/commons/PromptCard.tsx
+++ todoist/src/commons/PromptCard.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function PromptCard({ post, handleTagClick }: any) {
-  return <div>PromptCard</div>;
+  const { data: session } = useSession();
+  return (
+    <div className="bg-red-600">
+      <Image
+        src={post?.creator?.image}
+        width={40}
+        height={40}
+        alt="userimage"
+      />
+      {post?.creator?.username}
+      {post?.creator?.email}
+    </div>
+  );
 }
