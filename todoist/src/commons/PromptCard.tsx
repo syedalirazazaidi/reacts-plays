307fbab99fd06178a8c4d72e526af739ad87dad8
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-08-02 21:09:58 +0500

    add clipboard logic

--- todoist/src/commons/PromptCard.tsx
+++ todoist/src/commons/PromptCard.tsx
@@ -7,6 +7,7 @@ import { usePathname, useRouter } from "next/navigation";
 
 export default function PromptCard({ post, handleTagClick }: any) {
   const { data: session } = useSession();
+  const [copy, issetCopy] = useState(false);
   return (
     <div className="border-2 w-full md:w-[400px] p-4 m-2 flex flex-col md:flex-row items-center rounded-sm">
       <Image
@@ -23,13 +24,23 @@ export default function PromptCard({ post, handleTagClick }: any) {
         <p className="font-mono text-gray">{post?.creator?.email}</p>
       </div>
       <div className="ml-[26px] cursor-pointer">
-        <Image
-          src={"/assets/copy.svg"}
-          alt="copy"
-          width={16}
-          height={16}
-          className="text cursor-pointer "
-        />
+        {!copy ? (
+          <Image
+            src={"/assets/copy.svg"}
+            alt="copy"
+            width={20}
+            height={20}
+            className="text cursor-pointer "
+          />
+        ) : (
+          <Image
+            src={"/assets/tick.svg"}
+            alt="copy"
+            width={22}
+            height={22}
+            className="text cursor-pointer "
+          />
+        )}
       </div>
     </div>
   );
