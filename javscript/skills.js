9b463b5a0d1361cb7b7362553bef1901f4ad0567
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-08-08 12:16:44 +0500

    javascript tips and tricks

---
+++ javscript/skills.js
@@ -0,0 +1,21 @@
+
+// unlimited argument is Arrow & Regular functions//////
+
+
+// function b(...x) {
+//   console.log(x);
+// }
+// b(1, 2, 3, 4, 5);
+
+// const a = (...x) => console.log(x);
+// a(1, 2, 3);
+
+
+// get current urls//////
+
+// let _ = location.href
+// console.log(_)
+
+
+// javascript quick tip checking a value is a number
+
